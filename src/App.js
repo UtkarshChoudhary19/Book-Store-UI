0901edfe2117d51ebaca9fdf699c68f021592732
Utkarsh Choudhary <72205579+UtkarshChoudhary19@users.noreply.github.com>
2023-09-23 20:37:45 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+import './App.css';
+import NavBar from './Components/NavBar';
+import ImageGallery from './Components/ImageGallery';
+import BestSeller from './Components/BestSeller';
+
+
+function App() {
+
+  const [images,setImages] = useState([]);
+
+  return (
+    <div className='bg-primary'>
+      <main>
+        <div>
+        <NavBar  setImages={setImages}/>
+        </div>
+        <div className='bg-lightBlack'>
+          <BestSeller />
+        </div>
+      <div>
+      <ImageGallery images={images}/>
+      </div>
+
+      </main>
+    </div>
+  );
+}
+
+export default App;
